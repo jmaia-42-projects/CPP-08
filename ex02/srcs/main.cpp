b35a68e67582625e1dd0c265d3fb4158266ecec0
Jeremy Maia <jmaia@student.42.fr>
2022-11-03 15:04:20 +0100

    Remove useless line

--- ex02/srcs/main.cpp
+++ ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 17:05:59 by jmaia             #+#    #+#             */
-/*   Updated: 2022/06/09 17:06:35 by jmaia            ###   ########.fr       */
+/*   Updated: 2022/11/03 15:04:09 by jmaia            ###   ###               */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 int main()
 {
 	MutantStack<int> mstack;
+
 	mstack.push(5);
 	mstack.push(17);
 	std::cout << mstack.top() << std::endl;
@@ -25,7 +26,6 @@ int main()
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-	//[...]
 	mstack.push(0);
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
@@ -36,6 +36,5 @@ int main()
 		std::cout << *it << std::endl;
 		++it;
 	}
-	std::stack<int> s(mstack);
 	return 0;
 }
